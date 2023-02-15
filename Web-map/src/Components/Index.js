c65ed45715c3d557af361e68fe617ad6e2f7ed97
Paul Maweu <maweupaul85@gmail.com>
2023-02-15 21:24:34 +0300

    ..

---
+++ Web-map/src/Components/Index.js
@@ -0,0 +1,4 @@
+export { default as Login } from "../Pages/Login";
+export { default as Home } from "../Pages/Home";
+export { default as Signup } from "../Pages/SignUp";
+export { default as Google } from "../Utilities/LoginWithGoogle";
