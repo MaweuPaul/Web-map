3471c80ad6d4fcf47e79a7b78822767d5398f0e3
Paul Maweu <maweupaul85@gmail.com>
2023-02-16 18:34:44 +0300

    loading spinner

--- Web-map/src/App.jsx
+++ Web-map/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
-import { Login, Home, Signup } from "./Components/Index";
+import { Error, LoadingSpinner } from "./Components/Index";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
+import React, { lazy } from "react";
 
 function App() {
+  // to add lazy loading to provide feedback to the user
+  const Login = lazy(() => import("./Pages/Login"));
+  const SignUp = lazy(() => import("./Pages/SignUp"));
+  const Home = lazy(() => import("./Pages/Home"));
   return (
     <Box className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route
+          index
+          element={
+            <React.Suspense fallback={<LoadingSpinner />}>
+              <Login />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <React.Suspense fallback={<LoadingSpinner />}>
+              <Home />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <React.Suspense fallback={<LoadingSpinner />}>
+              <SignUp />
+            </React.Suspense>
+          }
+        />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Box>
   );
