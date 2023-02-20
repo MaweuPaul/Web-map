28c21b4ea900e3d8a525c433d7d0ae18d8a48078
Paul Maweu <maweupaul85@gmail.com>
2023-02-20 20:49:42 +0300

    ...

---
+++ Web-map/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import { Drawer } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+  return <Drawer></Drawer>;
+};
+
+export default Home;
