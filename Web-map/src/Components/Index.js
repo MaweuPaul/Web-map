c7c3608bb9c1ef1beba2f6ec0c78bf7bca4a1b0d
Paul Maweu <maweupaul85@gmail.com>
2023-02-22 07:49:52 +0300

    ....

--- Web-map/src/Components/Index.js
+++ Web-map/src/Components/Index.js
@@ -7,3 +7,6 @@ export { default as LoadingSpinner } from "./LoadingSpinner";
 export { default as LoginWithEmail } from "../Utilities/LoginWithEmail";
 export { default as LoginWithGoogle } from "../Utilities/LoginWithGoogle";
 export { default as CreateAccount } from "../Utilities/CreateAccount";
+export { default as Leaflet } from "../Pages/Leaflet";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
