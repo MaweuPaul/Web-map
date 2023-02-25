9d1d75e72b7480d47377e6869e978eecc858d552
MaweuPaul <maweupaul85@gmail.com>
2023-02-25 15:52:59 +0300

    ...

--- Web-map/src/App.jsx
+++ Web-map/src/App.jsx
@@ -1,58 +1,12 @@
-import {
-  Error,
-  LoadingSpinner,
-  Leaflet,
-  Info,
-  Footer,
-  Layers,
-} from "./Components/Index";
-import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
-import React, { lazy } from "react";
-
+import React, { useState } from "react";
+import RoutePages from "./Pages/Routes";
 function App() {
-  // to add lazy loading to provide feedback to the user
-  const Login = lazy(() => import("./Pages/Login"));
-  const SignUp = lazy(() => import("./Pages/SignUp"));
-  const Home = lazy(() => import("./Pages/Home"));
+  // to access the logged in user
+
   return (
     <Box className="App">
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
-            <React.Suspense fallback={<LoadingSpinner />}>
-              <Login />
-            </React.Suspense>
-          }
-        />
-
-        <Route
-          path="Home"
-          element={
-            <React.Suspense fallback={<LoadingSpinner />}>
-              <Home />
-            </React.Suspense>
-          }
-        >
-          {" "}
-          <Route path="info" element={<Info />} />
-          <Route path="leaflet" element={<Leaflet />} />
-          <Route path="layers" element={<Layers />} />
-        </Route>
-
-        <Route
-          path="/signup"
-          element={
-            <React.Suspense fallback={<LoadingSpinner />}>
-              <SignUp />
-            </React.Suspense>
-          }
-        />
-
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <RoutePages />
     </Box>
   );
 }
