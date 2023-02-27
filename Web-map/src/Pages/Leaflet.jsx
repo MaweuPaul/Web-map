00c84d9675b26ca5c53d84d5392ffe8c2caae1f4
MaweuPaul <maweupaul85@gmail.com>
2023-02-27 21:00:17 +0300

    added leaflet map with find my location capabilities

--- Web-map/src/Pages/Leaflet.jsx
+++ Web-map/src/Pages/Leaflet.jsx
@@ -1,11 +1,109 @@
-import { Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import {
+  Circle,
+  FeatureGroup,
+  LayerGroup,
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  Rectangle,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-const Leaflet = () => {
-  useEffect(() => {
-    document.title = "leaflet";
-  });
-  return <Typography color="#293745"> hello iam leaflet</Typography>;
-};
+export default function LeafletMap() {
+  const center = [-0.04830583139616625, 37.83410595020698];
+  const rectangle = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+  ];
 
-export default Leaflet;
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
+
+  return (
+    <div>
+      <MapContainer
+        center={center}
+        zoom={10}
+        style={{
+          width: "80vw",
+          height: "75vh",
+          paddingLeft: "4rem",
+          marginLeft: "10rem",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <button
+          onClick={() => {
+            map.locate();
+          }}
+          style={{
+            position: "absolute",
+            top: "80px",
+            left: "10px",
+            zIndex: "1000",
+          }}
+        >
+          Find my location
+        </button>
+
+        <LayersControl position="topright">
+          <LayersControl.Overlay name="Marker with popup">
+            <Marker position={center}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay checked name="Layer group with circles">
+            <LayerGroup>
+              <Circle
+                center={center}
+                pathOptions={{ fillColor: "blue" }}
+                radius={200}
+              />
+              <Circle
+                center={center}
+                pathOptions={{ fillColor: "red" }}
+                radius={100}
+                stroke={false}
+              />
+              <LayerGroup>
+                <Circle
+                  center={[51.51, -0.08]}
+                  pathOptions={{ color: "green", fillColor: "green" }}
+                  radius={100}
+                />
+              </LayerGroup>
+            </LayerGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
+
+        <LocationMarker />
+      </MapContainer>
+    </div>
+  );
+}
